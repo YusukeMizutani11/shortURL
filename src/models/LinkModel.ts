21814b863c496052bd1594eb3947ec33d411e4c3
YusukeMizutani11 <yusuke.mizutani@smail.astate.edu>
2023-03-30 05:59:26 -0500

    checkpoint 5

--- src/models/LinkModel.ts
+++ src/models/LinkModel.ts
@@ -1,7 +1,17 @@
+import { createHash } from 'crypto';
 import { AppDataSource } from '../dataSource';
 import { Link } from '../entities/Link';
 
 const linkRepository = AppDataSource.getRepository(Link);
+
+// const md5 = createHash('md5');
+// md5.update('https://youtube.com/watch?v=dQw4w9WgXcQ');
+// const urlHash = md5.digest('base64url');
+// const shortLinkId = urlHash.slice(0, 9);
+
+// console.log(`MD5 Hash: ${urlHash}`);
+// console.log(`linkId: ${shortLinkId}`);
+// console.log(`MD5 Hash: ${urlHash}`);
 
 async function getLinkById(linkId: string): Promise<Link | null> {
   const link = await linkRepository
@@ -12,4 +22,13 @@ async function getLinkById(linkId: string): Promise<Link | null> {
   return link;
 }
 
-export { getLinkById };
+function createLinkId(originalUrl: string, userId: string): string {
+  const md5 = createHash('md5');
+  md5.update(originalUrl + userId);
+  const urlHash = md5.digest('base64url');
+  const linkId = urlHash.slice(0, 9);
+
+  return linkId;
+}
+
+export { getLinkById, createLinkId };
